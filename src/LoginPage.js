2aa6b64dd6d06342a9b55a4f3b1f30fa17b612fb
hoodjustin <82976113+hoodjustin@users.noreply.github.com>
2022-05-24 10:47:02 -0700

    updated style of login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,12 +1,40 @@
 import "./LoginPage.css";
 import {header, loginUrl} from "./spotify";
+import hearts from './hearts-pink-hearts.gif'
+import rose from './rose1.gif'
 
 function LoginPage() {
+    document.body.style = 'background-image: linear-gradient(pink, white);';
     return (
         <div className="login">
             {header}
 
-            <a href={loginUrl} id="signInButton">Sign in with Spotify!</a>
+            <a href={loginUrl} id="signInButton">oo Sign in with Spotify! Your future awaits oo</a>
+            <h1>-xoxo matchmakers of dev</h1>
+            <img src={rose} alt="rose" style={{
+                position: 'absolute',
+                height: '250px',
+                left: 800,
+                top: 400
+            }}/>
+            <img src={hearts} alt="loading..." 
+            style={{
+                position: 'absolute',
+                height: '100%',
+                top: '0',
+                right: '0',
+                zIndex: 2
+            }}
+            />
+            <img src={hearts} alt="loading..." 
+            style={{
+                position: 'absolute',
+                height: '100%',
+                top: '0',
+                left: "0",
+                zIndex: 2
+            }}
+            />
         </div>
     );
 }
